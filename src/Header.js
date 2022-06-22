c189728c3560c5e43694bc727bcc3fe868d5f6f8
Maxim Remez <remezmaxim1992@gmail.com>
2022-06-22 11:18:15 +0300

    first commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <a href='#s'>Home</a>
+            <a href='#s'>New Feed</a>
+            <a href='#s'>Messages</a>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/Technologies.js
@@ -0,0 +1,14 @@
+
+
+const Technologies = () =>{
+    return(
+        <ul>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>JS</li>
+            <li>React</li>
+        </ul>
+    );
+}
+
+export default Technologies;
